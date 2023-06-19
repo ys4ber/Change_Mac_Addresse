f9099cf1eba116bf924437f31affd42cd57be03a
Youssef Saber <youssef.saber6464@outlook.com>
2023-06-19 15:46:55 +0100

    first commit

---
+++ mac.py
@@ -0,0 +1,20 @@
+import os
+import random
+import string
+
+interface = 'wlp3s0'
+
+def random_mac():
+
+
+    mac = [0x00, 0x16, 0x3e,
+           random.randint(0x00,0x7f),
+           random.randint(0x00,0xff),
+           random.randint(0x00,0xff)]
+    return ':'.join(format(x, '02x') for x in mac)
+
+
+
+new_mac = random_mac()
+
+os.system(f"sudo ifconfig {interface} hw ether {new_mac}")
